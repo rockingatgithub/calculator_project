5ea0206b0507cec6449b582ce691c9d3598fe562
rockingatgithub <singhatgithub@users.noreply.github.com>
2020-05-02 01:58:16 +0530

    added js

---
+++ index.js
@@ -0,0 +1,33 @@
+var a=0;
+var b=0;
+function print(){
+    document.getElementById('display-box').innerHTML=7;
+}
+//function for addition
+function add(a, b)
+{
+    a= a + b;
+    return a;
+}
+
+//function for substraction
+function subs(a, b)
+{
+    a= a + b;
+    return a;
+}
+//function for multiply
+
+function multiply(a, b)
+{
+    a= a * b;
+    return a;
+}
+
+//function for divison
+
+function divison(a, b)
+{
+    a= a / b;
+    return a;
+}
