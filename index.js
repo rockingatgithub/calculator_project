745cf94284095c1f7ed379a69f5b09464b1c3d93
rockingatgithub <singhatgithub@users.noreply.github.com>
2020-05-02 20:33:11 +0530

    calculator function added

--- index.js
+++ index.js
@@ -1,33 +1,165 @@
-var a=0;
-var b=0;
-function print(){
+var a=Infinity;
+var b=Infinity;
+
+//now for all the number keys....
+function printone(){
+    if(a==Infinity)
+    {
+        a=1;
+    }
+    else
+    {
+        b=1;
+    }
+    document.getElementById('display-box').innerHTML=1;
+}
+function printtwo(){
+    if(a==Infinity)
+    {
+        a=2;
+    }
+    else
+    {
+        b=2;
+    }
+    document.getElementById('display-box').innerHTML=2;
+}
+function printthree(){
+    if(a==Infinity)
+    {
+        a=3;
+    }
+    else
+    {
+        b=3;
+    }
+    document.getElementById('display-box').innerHTML=3;
+}
+function printfour(){
+    if(a==Infinity)
+    {
+        a=4;
+    }
+    else
+    {
+        b=4;
+    }
+    document.getElementById('display-box').innerHTML=4;
+}
+function printfive(){
+    if(a==Infinity)
+    {
+        a=5;
+    }
+    else
+    {
+        b=5;
+    }
+    document.getElementById('display-box').innerHTML=5;
+}
+function printsix(){
+    if(a==Infinity)
+    {
+        a=6;
+    }
+    else
+    {
+        b=6;
+    }
+    document.getElementById('display-box').innerHTML=6;
+}
+function printseven(){
+    if(a==Infinity)
+    {
+        a=7;
+    }
+    else
+    {
+        b=7;
+    }
     document.getElementById('display-box').innerHTML=7;
 }
+function printeight(){
+    if(a==Infinity)
+    {
+        a=8;
+    }
+    else
+    {
+        b=8;
+    }
+    document.getElementById('display-box').innerHTML=8;
+}
+function printnine(){
+    if(a==Infinity)
+    {
+        a=9;
+    }
+    else
+    {
+        b=9;
+    }
+    document.getElementById('display-box').innerHTML=9;
+}
+function printzero(){
+    if(a==Infinity)
+    {
+        a=0;
+    }
+    else
+    {
+        b=0;
+    }
+    document.getElementById('display-box').innerHTML=0;
+}
+function printten(){
+    if(a==Infinity)
+    {
+        a=10;
+    }
+    else
+    {
+        b=10;
+    }
+    document.getElementById('display-box').innerHTML=10;
+}
+
 //function for addition
-function add(a, b)
+function funadd()
 {
-    a= a + b;
-    return a;
+    a= a + b ;
 }
 
 //function for substraction
-function subs(a, b)
+function funsub()
 {
-    a= a + b;
-    return a;
+    a= a - b;
 }
 //function for multiply
 
-function multiply(a, b)
+function funmul()
 {
     a= a * b;
-    return a;
 }
-
 //function for divison
 
-function divison(a, b)
+function fundiv()
 {
     a= a / b;
-    return a;
+}
+//function for equal.
+
+function funequal()
+{
+    document.getElementById('display-box').innerHTML=a;
+}
+
+//function for all clear.....
+
+function funac()
+{
+    //reset a and b....
+    a=Infinity;
+    b=Infinity;
+    document.getElementById('display-box').innerHTML="";
 }
